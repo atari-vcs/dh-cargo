df688ce54e36717aeda759a07061aaf59bb2fb81
Ximin Luo <infinity0@debian.org>
2018-12-18 17:57:24 -0800

    Import Debian version 14
    dh-cargo (14) unstable; urgency=medium
    
      * Use absolute path when dpkg-searching for libs.
    
    dh-cargo (13) unstable; urgency=medium
    
      * Make debian/cargo_registry behaviour more robust.
      * Make dh-cargo-built-using behaviour more robust.
    
    dh-cargo (12) unstable; urgency=medium
    
      * Fix typo that broke installation of binary crates.
    
    dh-cargo (11) unstable; urgency=medium
    
      * Reimplement in terms of /usr/share/cargo/bin/cargo wrapper script.
      * dh-cargo-built-using: fix bug with crates that build >1 static lib.
    
    dh-cargo (10) unstable; urgency=medium
    
      * Fix usage of "sub install", it takes an extra destdir param.
    
    dh-cargo (9) unstable; urgency=medium
    
      * Set remap-path-prefix for reproducible builds.
      * Support adding extra flags to dh_auto_test / dh_auto_install.
    
    dh-cargo (8) unstable; urgency=medium
    
      * Generate ${cargo:Built-Using} and ${cargo:X-Cargo-Built-Using} substvars.
    
    dh-cargo (7) unstable; urgency=medium
    
      * Be more verbose, as recommended by Policy.
      * Update to latest Standards-Version.
      * Set CARGO_TARGET_DIR so build products are saved.
      * If there was a custom build output, symlink it to cargo_out_dir.
    
    dh-cargo (6) unstable; urgency=medium
    
      * Don't add .pc directories to binary package, but do add debian/patches.
        (Closes: #880689)
    
    dh-cargo (5) unstable; urgency=medium
    
      * Pass `--cap-lints warn` in rustflags to avoid builds breaking when rustc
        adds new warnings.
      * Work around a stupid ftpmaster rule about files with old dates.
    
    dh-cargo (4) unstable; urgency=medium
    
      * Pass cargo-version not Debian-version to `cargo install`.
      * Pass debuginfo=2 and LDFLAGS when building.
      * Add other flags to support cross-compiling.
    
    dh-cargo (3) unstable; urgency=medium
    
      * Update to latest Standards-Version; no changes required.
      * Run `cargo build` during dh_auto_test to check that each library package
        actually builds, e.g. to ensure that any non-rust system dependencies were
        correctly expressed.
    
    dh-cargo (2) unstable; urgency=medium
    
      [ Josh Triplett ]
      * Update package description with additional documentation.
    
      [ Ximin Luo ]
      * Upload to unstable.
    
    dh-cargo (1) experimental; urgency=medium
    
      * Initial Release.

---
+++ cargo.pm
@@ -0,0 +1,188 @@
+# debhelper buildsystem for Rust crates using Cargo
+#
+# Josh Triplett <josh@joshtriplett.org>
+# Ximin Luo <infinity0@debian.org>
+
+package Debian::Debhelper::Buildsystem::cargo;
+
+use strict;
+use warnings;
+use Cwd;
+use Debian::Debhelper::Dh_Lib;
+use Dpkg::Changelog::Debian;
+use Dpkg::Control::Info;
+use Dpkg::Version;
+use JSON::PP;
+use base 'Debian::Debhelper::Buildsystem';
+
+sub DESCRIPTION {
+    "Rust Cargo"
+}
+
+sub cargo_version {
+    my $src = shift;
+    open(F, "cargo metadata --manifest-path $src --no-deps --format-version 1 |");
+    local $/;
+    my $json = JSON::PP->new;
+    my $manifest = $json->decode(<F>);
+    return %{@{%{$manifest}{'packages'}}[0]}{'version'} . "";
+}
+
+sub deb_host_rust_type {
+    open(F, 'printf "include /usr/share/rustc/architecture.mk\n\
+all:\n\
+	echo \$(DEB_HOST_RUST_TYPE)\n\
+" | make --no-print-directory -sf - |');
+    $_ = <F>;
+    chomp;
+    return $_;
+}
+
+sub check_auto_buildable {
+    my $this = shift;
+    if (-f $this->get_sourcepath("Cargo.toml")) {
+        return 1;
+    }
+    return 0;
+}
+
+sub new {
+    my $class = shift;
+    my $this = $class->SUPER::new(@_);
+    $this->enforce_in_source_building();
+    return $this;
+}
+
+sub pre_building_step {
+    my $this = shift;
+    my $step = shift;
+
+    $this->{cargo_home} = Cwd::abs_path($this->get_sourcepath("debian/cargo_home"));
+    $this->{host_rust_type} = deb_host_rust_type;
+
+    my $control = Dpkg::Control::Info->new();
+
+    my $source = $control->get_source();
+    my $crate = $source->{'X-Cargo-Crate'};
+    if (!$crate) {
+        $crate = $source->{Source};
+        $crate =~ s/^rust-//;
+        $crate =~ s/-[0-9]+(\.[0-9]+)*$//;
+    }
+    $this->{crate} = $crate;
+    my $changelog = Dpkg::Changelog::Debian->new(range => { count => 1 });
+    $changelog->load($this->get_sourcepath("debian/changelog"));
+    $this->{version} = Dpkg::Version->new(@{$changelog}[0]->get_version())->version();
+
+    my @packages = $control->get_packages();
+    $this->{libpkg} = 0;
+    $this->{binpkg} = 0;
+    $this->{featurepkg} = [];
+    foreach my $package (@packages) {
+        if ($package->{Package} =~ /^librust-.*-dev$/) {
+            if ($package->{Package} =~ /\+/) {
+                push(@{$this->{featurepkg}}, $package->{Package});
+                next;
+            }
+            if ($this->{libpkg}) {
+                error("Multiple Cargo lib packages found: " . $this->{libpkg} . " and " . $package->{Package});
+            }
+            $this->{libpkg} = $package->{Package};
+        } elsif ($package->{Architecture} ne 'all') {
+            $this->{binpkg} = $package->{Package};
+        }
+    }
+    if (!$this->{libpkg} && !$this->{binpkg}) {
+        error("Could not find any Cargo lib or bin packages to build.");
+    }
+    if (@{$this->{featurepkg}} && !$this->{libpkg}) {
+        error("Found feature packages but no lib package.");
+    }
+
+    my $parallel = $this->get_parallel();
+    $this->{j} = $parallel > 0 ? ["-j$parallel"] : [];
+
+    $ENV{'CARGO_HOME'} = $this->{cargo_home};
+    $ENV{'DEB_CARGO_CRATE'} = $crate . '_' . $this->{version};
+    $ENV{'DEB_HOST_RUST_TYPE'} = $this->{host_rust_type};
+    $ENV{'DEB_HOST_GNU_TYPE'} = dpkg_architecture_value("DEB_HOST_GNU_TYPE");
+
+    $this->SUPER::pre_building_step($step);
+}
+
+sub get_sources {
+    my $this=shift;
+    opendir(my $dirhandle, $this->get_sourcedir());
+    my @sources = grep { $_ ne '.' && $_ ne '..' && $_ ne '.git' && $_ ne '.pc' && $_ ne 'debian' } readdir($dirhandle);
+    push @sources, 'debian/patches' if -d $this->get_sourcedir() . '/debian/patches';
+    closedir($dirhandle);
+    @sources
+}
+
+sub configure {
+    my $this=shift;
+    doit("cp", $this->get_sourcepath("debian/cargo-checksum.json"),
+               $this->get_sourcepath(".cargo-checksum.json"));
+    if (-e "/usr/share/cargo/registry") {
+        doit("ln", "-sfT", "/usr/share/cargo/registry", "debian/cargo_registry");
+    } else {
+        doit("mkdir", "-p", "debian/cargo_registry"); # empty registry
+    }
+    doit("/usr/share/cargo/bin/cargo", "prepare-debian", "debian/cargo_registry");
+}
+
+sub test {
+    my $this=shift;
+    my $cmd="build";
+    if (!defined $_[0]) {
+        # nop
+    } elsif ($_[0] eq "test") {
+        $cmd="test";
+        shift;
+    } elsif ($_[0] eq "build") {
+        shift;
+    }
+    # Check that the thing compiles. This might fail if e.g. the package
+    # requires non-rust system dependencies and the maintainer didn't provide
+    # this additional information to debcargo.
+    doit("/usr/share/cargo/bin/cargo", $cmd, @_);
+    # test generating Built-Using fields
+    doit("env", "CARGO_CHANNEL=debug", "/usr/share/cargo/bin/dh-cargo-built-using");
+}
+
+sub install {
+    my $this=shift;
+    my $destdir=shift;
+    my $crate = $this->{crate} . '-' . $this->{version};
+    if ($this->{libpkg}) {
+        my $target = $this->get_sourcepath("debian/" . $this->{libpkg} . "/usr/share/cargo/registry/$crate");
+        my @sources = $this->get_sources();
+        doit("mkdir", "-p", $target);
+        doit("cp", "--parents", "-at", $target, @sources);
+        doit("rm", "-rf", "$target/target");
+        doit("cp", $this->get_sourcepath("debian/cargo-checksum.json"), "$target/.cargo-checksum.json");
+        # work around some stupid ftpmaster rule about files with old dates.
+        doit("touch", "-d@" . $ENV{SOURCE_DATE_EPOCH}, "$target/Cargo.toml");
+    }
+    foreach my $pkg (@{$this->{featurepkg}}) {
+        my $target = $this->get_sourcepath("debian/$pkg/usr/share/doc");
+        doit("mkdir", "-p", $target);
+        doit("ln", "-s", $this->{libpkg}, "$target/$pkg");
+    }
+    if ($this->{binpkg}) {
+        # Do the install
+        doit("env", "DEB_CARGO_PACKAGE=$this->{binpkg}",
+             "/usr/share/cargo/bin/cargo", "install", @_);
+        # generate Built-Using fields
+        doit("/usr/share/cargo/bin/dh-cargo-built-using", $this->{binpkg});
+    }
+}
+
+sub clean {
+    my $this=shift;
+    doit("/usr/share/cargo/bin/cargo", "clean", @_);
+    doit("rm", "-rf", $this->get_sourcepath(".cargo-checksum.json"));
+    doit("rm", "-rf", "debian/cargo_registry");
+}
+
+1
